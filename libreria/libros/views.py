9ba00a296fab43c013da9ac38334d3b0148aa91d
Zardoc9688 <juancamiloma17@gmail.com>
2022-09-01 06:47:25 -0500

    a

--- libreria/libros/views.py
+++ libreria/libros/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+#imports
+from django.template import loader
+from django.http import HttpResponse
+
+
+def index(request):
+    #Archivo HTML con template
+    template = loader.get_template('index.html')
+    #logica de la vista
+    context = {}
+    #respuesta
+    return HttpResponse(template.render(context,request))
